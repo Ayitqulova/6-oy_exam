3f4c7f5b32cae27baf0fb61e58bdb90ed20eac83
Ayitqulova <ayitqulovaguliza@gmail.com>
2026-04-26 22:56:58 +0500

    Update

--- src/components/fullstack/Fullstack2.jsx
+++
@@ -1,7 +0,0 @@
-export default function fullstack2(){
-    return(
-        <section className="w-[80%] mx-auto py-10">
-            <div className="w-full  bg-[#ECF8F9] "></div>
-        </section>
-    )
-}
